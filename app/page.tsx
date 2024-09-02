d5c739fff07c99ec6f97ccdcaad3297a6968cc7d
Keyvan <skhprogramming@gmail.com>
2024-09-02 17:55:00 +0330

    feat: added recent posts to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import Intro from '@/components/intro';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import RecentPosts from '@/components/recent-posts';
 
 export default function Home() {
-  const markdown = `
-    # This is a markdown heading
-  `;
-
   return (
     <main className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
-        <MDXRemote source={markdown} />
+
+        <RecentPosts />
       </div>
     </main>
   );
