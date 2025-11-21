9032731a4506653bd7a43b0e8fd2b420f9fc0986
skh113 <skhprogramming@gmail.com>
2025-11-21 12:56:34 +0330

    refactor: reposition Analytics and SpeedInsights components within RootLayout for better performance tracking and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({
           <Header />
           <main className='grow'>{children}</main>
           <Footer />
-          <Analytics />
-          <SpeedInsights />
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
