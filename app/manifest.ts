ff5a641d6011c6eb344cd5ee470f98596a60ee84
skh113 <skhprogramming@gmail.com>
2026-08-04 14:14:16 +0330

    refactor: use static images instead of creating them on the fly

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,25 +1,31 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Seyed Keyvan Hosseini | Portfolio',
-    short_name: 'SKH Portfolio',
-    description: 'Personal portfolio and blog of Seyed Keyvan Hosseini.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#ffffff',
+    name: "Seyed Keyvan Hosseini | Portfolio",
+    short_name: "SKH Portfolio",
+    description: "Personal portfolio and blog of Seyed Keyvan Hosseini.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: '/icon/192',
-        sizes: '192x192',
-        type: 'image/png'
+        src: "/images/icons/192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/icon/512',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
+        src: "/images/icons/512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/images/icons/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
   };
 }
